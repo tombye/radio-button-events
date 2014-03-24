364ab79aac2838c97e23edc9e715bdfb5640663e
Tom Byers <tombaromba@gmail.com>
2014-03-24 19:53:07 +0000

    Split messaging out into separate function

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,6 +1,7 @@
 var form = document.getElementsByTagName('form')[0],
     results = document.getElementById('results'),
-    addEvent;
+    addEvent,
+    printResult;
 
 addEvent = function (elm, event, callback) {
   if (elm.addEventListener) {
@@ -10,13 +11,16 @@ addEvent = function (elm, event, callback) {
   }
 };
 
+printResult = function (result) {
+  var resultsTxt = document.createTextNode(result);
+  results.innerHTML = "";
+  results.appendChild(resultsTxt);
+};
+
 addEvent(form, 'change', function (e) {
-  var target = (e.target) ? e.target : e.srcElement,
-      resultsTxt;
+  var target = (e.target) ? e.target : e.srcElement;
 
   if (target.type.toLowerCase() === 'radio') {
-    resultsTxt = document.createTextNode("Change called on radio with id of " + target.id);
-    results.innerHTML = "";
-    results.appendChild(resultsTxt);
+    printResult("Change called on radio with id of " + target.id);
   }
 });
